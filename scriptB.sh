48aa2894a0732d7a71fb9c0b640aa7d5f6002e69
Iryna <123497292+SkhtskIryna@users.noreply.github.com>
2024-12-15 16:07:11 +0200

    Update scripts

--- scriptB.sh
+++ scriptB.sh
@@ -8,8 +8,18 @@ while true; do
     sleep "$sleep_time"
     echo "Sleeping for $sleep_time seconds..."
 
-    # Call the function to make a request asynchronously
+    # Measure start time
+    start_time=$(date +%s%3N)
+
+    # Call the function to make a request
     curl -i -X GET 127.0.0.1/compute
+
+    # Measure end time
+    end_time=$(date +%s%3N)
+
+    # Calculate the response time
+    response_time=$((end_time - start_time))
     echo "Request sent at $(date)"
+    echo "Response time: ${response_time} ms"
     echo "---------------------------------------"
 done
